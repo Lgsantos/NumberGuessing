77c582b96ccf6af3fba8bf9dae04045105d15fed
Luís Gustavo dos Santos <luis.gustavo.santos@gmail.com>
2022-05-11 17:47:38 -0300

    Created script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let computerGuess;
+
+function init() {
+    computerGuess = Math.floor(Math.random() * 100 + 1);
+    // console.log(computerGuess);
+    document.getElementById("newGameButton").style.display = "none";
+    document.getElementById("gameArea").style.display = "none";
+}
+
+function startGameView() {
+    document.getElementById("welcomeScreen").style.display = "none";
+    document.getElementById("gameArea").style.display = "block";
+}
+
+function easyMode() {
+    startGameView();
+}
+
+function hardMode() {
+    startGameView();
+}
